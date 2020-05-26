0f45a080f09af770259ca931c355c96f8a00bb84
danielxurbina <danielur1999@gmail.com>
2020-05-26 10:01:48 -0400

    backend

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,4 +3,15 @@ class EventsController < ApplicationController
         events = Event.all
         render json: EventSerializer.new(events)
     end
+
+    def create
+        event = Event.create(events_params)
+        render json: event
+    end
+
+    private
+
+    def events_params
+        params.require(:event).permit(:title, :location, :description, :image_url, :price, :date)
+    end
 end
